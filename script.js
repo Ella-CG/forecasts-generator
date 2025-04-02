6f012566c2e3ddf8aeaebd344248e6fb026805ef
Ella-CG <ella23050@yandex.ru>
2025-04-02 11:01:37 +0300

    Домашнее задание 5
    Создала функцию- генератор числа, а на ее основе функцию-генератор предсказания. Все предсказания отображаются в листе предсказаний.

--- script.js
+++ script.js
@@ -1,3 +1,56 @@
+const forecastBtn = document.querySelector('.forecast-btn');
+const forecast = document.querySelector('.current-forecast h1');
+const forecastProbability = document.querySelector('.current-forecast p')
+const forecastsContainer = document.querySelector('.forecasts')
+
+function getRandomNumber(min, max) {
+
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+forecastBtn.addEventListener('click', function() {
+    const forecastNumber = getRandomNumber(1, 4);
+
+    function doForecast() {
+
+        let predictionNumber = forecastNumber;
+        let predictionText = "";
+
+        if (predictionNumber == 1) {
+            predictionText = "Сегодня будет отличный день!";
+        } else if (predictionNumber == 2) {
+            predictionText = "Твое желание исполнится!";
+        } else {
+            predictionText = "Ты поедешь в прекрасное путешествие!";
+        }
+        return predictionText;
+    }
+
+    const newForecast = doForecast(forecastNumber);
+    forecast.textContent = newForecast;
+
+    const newProbability = getRandomNumber(1, 100);
+    forecastProbability.textContent = `Вероятность: ${newProbability}  %`;
+
+    const forecastItem = document.querySelector("#forecast-item");
+
+    function makeForecastByTemplate(newForecast, newProbability) {
+        const myForecast = forecastItem.content.cloneNode(true);
+
+        myForecast.querySelector('h3').textContent = newForecast;
+        myForecast.querySelector('p').textContent = `Вероятность: ${newProbability}  %`;
+
+        return myForecast;
+    }
+
+    const forecastList = makeForecastByTemplate(newForecast, newProbability);
+    forecastsContainer.prepend(forecastList);
+
+
+
+})
+
+
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
